bdff1887cf2443cb02e2b3f8c7d94331af977f5c
juliandos <95juliandos@gmail.com>
2025-09-30 16:55:52 -0500

    tipos de letra archivo

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import LandingPage from "./pages/LandingPage";
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <LandingPage />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* Más rutas cuando las tengas */}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
