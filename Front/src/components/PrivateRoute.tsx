9a364fb07c7103340a7f6244df5d55309930a66f
juliandos <95juliandos@gmail.com>
2025-10-01 09:06:30 -0500

    ruta privada

---
+++ Front/src/components/PrivateRoute.tsx
@@ -0,0 +1,16 @@
+// src/components/PrivateRoute.tsx
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+interface PrivateRouteProps {
+  children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const { currentUser } = useAuth();
+  
+  return currentUser ? <>{children}</> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
