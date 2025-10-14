7890d1b72871a602c229f395cd577625a6dca6cb
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-10-13 23:20:25 -0500

    index de componentes para formularios de creación

---
+++ src/components/forms/index.ts
@@ -0,0 +1,11 @@
+// src/components/forms/index.ts
+// Exportación centralizada de todos los formularios
+
+export { default as MedicoForm } from './MedicoForm';
+export type { MedicoFormData } from './MedicoForm';
+
+export { default as PacienteForm } from './PacienteForm';
+export type { PacienteFormData } from './PacienteForm';
+
+export { default as CitaForm } from './CitaForm';
+export type { CitaFormData } from './CitaForm';
